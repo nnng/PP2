b86d7553431ce402efae1763682e67f96aa64744
Gordeev Nikita <nnn.gordeev@gmail.com>
2026-01-26 21:09:52 +0500

    Added Practice1 file with pythin-basics file with 6 examples in it:hello_world.py , variables.py, data_types.py, numbers.py, casting.py, strings.py

---
+++ Practice1/python-basics/data_types.py
@@ -0,0 +1,29 @@
+# Example №1
+
+x = 5
+print(type(x))
+print(x)
+
+
+# Example №2
+
+x = "Hello World"
+print(x)
+
+
+# Example №3
+
+x = frozenset({"apple", "banana", "cherry"})
+print(x)
+
+
+# Example №4
+
+x = None
+print(x)
+
+
+# Example №5
+
+x = 1j
+print(x)
---
+++ Practice1/python-basics/strings.py
@@ -0,0 +1,35 @@
+# Example №1
+
+print("Hello")
+print('Hello')
+
+
+# Example №2
+
+print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
+
+
+# Example №3
+
+a = "Hello"
+print(a)
+
+
+# Example №4
+
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+
+
+# Example №5
+
+a = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+print(a)
